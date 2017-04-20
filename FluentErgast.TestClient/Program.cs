b442c73dda5178569e17f02c1e406d8fdde58d5a
Lewis Henson <lewis.henson@gmail.com>
2017-04-20 20:54:07 +0100

    Output standings in more readable format.

--- FluentErgast.TestClient/Program.cs
+++ FluentErgast.TestClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using FluentErgast.F1.Dtos.DriverStandings;
 
 namespace FluentErgast.TestClient
 {
@@ -13,11 +14,33 @@ namespace FluentErgast.TestClient
 
         private static async Task MainAsync()
         {
-            var standings = await FluentErgast.F1.DriverStandings.ForRound(5).ForYearAsync(2009);
+            var standings = await FluentErgast.F1.DriverStandings.ForYearAsync(2009);
 
-            var json = JsonConvert.SerializeObject(standings);
+            OutputStandings(standings);
+        }
 
-            Console.WriteLine(json);
+        private static void OutputStandings(StandingsTable standings)
+        {
+            Console.WriteLine($"Formula 1 Driver Standings ({standings.Season} season)");
+
+            foreach (var standingsList in standings.StandingsLists)
+            {
+                foreach (var driverStanding in standingsList.DriverStandings)
+                {
+                    OutputDriver(driverStanding);
+                }
+            }
+        }
+
+        private static void OutputDriver(DriverStanding driver)
+        {
+            Console.Write($"{driver.PositionText.PadLeft(2, ' ')} > ");
+
+            var driverName = $"{driver.Driver.GivenName} {driver.Driver.FamilyName} ({driver.Constructors.Last().Name})";                Console.Write(driverName.PadRight(35, ' '));
+
+            Console.Write($"{driver.Points} points");
+
+            Console.WriteLine();
         }
     }
 }
